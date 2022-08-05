0c82b30e60dbb1288c0306cafceb0b796b7102f5
Galaktikos <galaktikosofficial@gmail.com>
2022-08-04 20:36:30 -0400

    Initial Commit
    Includes project and solution files as well as much of the base code for the protocol.

---
+++ Manager.cs
@@ -0,0 +1,58 @@
+﻿namespace MPMP;
+
+public class Manager
+{
+    public delegate void ReadCompleteEvent(byte[] data);
+
+    /// <summary>
+    /// Called after all modules complete a read.
+    /// </summary>
+    public event ReadCompleteEvent? OnReadComplete;
+
+    public delegate void BuildCompleteEvent(byte[] data);
+
+    /// <summary>
+    /// Called after all modules complete a build.
+    /// </summary>
+    public event BuildCompleteEvent? OnBuildComplete;
+
+    private Module[] _Modules;
+    public Module[] Modules
+    {
+        get => _Modules;
+        set
+        {
+            for (int i = 0; i < value.Length; i++)
+                value[i].Index = i;
+
+            _Modules = value;
+        }
+    }
+
+    public Manager(Module[] modules)
+    {
+        Modules = modules;
+    }
+
+    public void Read(byte[] data) =>
+        Read(data, 0);
+
+    internal void Read(byte[] data, int index)
+    {
+        if (index == Modules.Length)
+            OnReadComplete?.Invoke(data);
+        else
+            Modules[index].Read(data);
+    }
+
+    public void Build(byte[] data) =>
+        Build(data, 0);
+
+    internal void Build(byte[] data, int index)
+    {
+        if (index == Modules.Length)
+            OnBuildComplete?.Invoke(data);
+        else
+            Modules[index].Build(data);
+    }
+}
---
+++ Module.cs
@@ -0,0 +1,28 @@
+﻿namespace MPMP;
+
+public abstract class Module
+{
+    internal int Index;
+
+    private readonly Manager Manager;
+
+    public Module(Manager manager) =>
+        Manager = manager;
+
+    /// <summary>
+    /// Passes a convert request to the following module.
+    /// </summary>
+    /// <param name="data"></param>
+    public void ContinueRead(byte[] data) =>
+        Manager.Read(data, Index + 1);
+
+    /// <summary>
+    /// Passes a build request to the following module.
+    /// </summary>
+    /// <param name="data"></param>
+    public void ContinueBuild(byte[] data) =>
+        Manager.Build(data, Index + 1);
+
+    public abstract void Read(byte[] data);
+    public abstract void Build(byte[] data);
+}
